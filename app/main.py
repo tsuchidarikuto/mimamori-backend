072b0140f561727d6feb582d598c8278ad80ec42
rick08698 <teraoka182@gmail.com>
2025-07-13 06:02:40 +0900

    DB追加

--- app/main.py
+++ app/main.py
@@ -2,26 +2,10 @@ from fastapi import FastAPI
 from api import router
 from config import settings
 
-app = FastAPI(title="Voice Assistant API")
+server = FastAPI(title="Voice Assistant API")
 
-app.include_router(router)
-
-<<<<<<< HEAD
-#test
-@app.get("/")
-async def root():
-    return {"message": "Hello, Teraoka!"}
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    # WebSocket処理をここに書く
-    #test 
-=======
->>>>>>> origin/dev
+server.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host=settings.host, port=settings.port)
-
-    
+    uvicorn.run(server, host=settings.host, port=settings.port)
