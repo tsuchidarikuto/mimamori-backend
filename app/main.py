0bfe29c9c6e9bb37156793a3399b10d6ca8922cd
tsuchidarikuto <tsuchidarikuto@gmail.com>
2025-07-13 03:41:53 +0900

    CORSをいったん削除

--- app/main.py
+++ app/main.py
@@ -4,17 +4,6 @@ from config import settings
 
 app = FastAPI(title="Voice Assistant API")
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-
 app.include_router(router)
 
 
